cb2eae9ead472bf12a71227660f4abd28ee51975
Key <selpech@gmail.com>
2020-03-18 10:49:25 +0100

    Title have been changed, Peach top tier yes

--- START/inc/header.php
+++ START/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>For Edu or Org Design</title>
+    <title>Oui, Peach top tier, oui.</title>
     <link href="css/style.css" rel="stylesheet">
 
 </head>
