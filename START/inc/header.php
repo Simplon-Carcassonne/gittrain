5a72555097e67a7208681568c94375e201281fa9
alto971 <gwadaldesign@gmail.com>
2020-03-18 11:44:16 +0100

    chgt ds header.php

--- START/inc/header.php
+++ START/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Oui, Peach top tier, oui.</title>
+    <title>hello title</title>
     <link href="css/style.css" rel="stylesheet">
 
 </head>
