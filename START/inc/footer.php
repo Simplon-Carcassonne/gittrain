b5fe24b46939277a4131def5370a6676ec1657c8
alto971 <mafabrikjeux@gmail.com>
2020-03-18 09:27:54 +0100

    first commit

---
+++ START/inc/footer.php
@@ -0,0 +1,41 @@
+ <!-- Footer -->
+ <div style="background-color: #EAAF47; padding: 20px; text-align: center;">
+     <h5 style="margin-bottom: 10px;">Follow us on our social media accounts</h5>
+     <div style="padding-bottom: 10px; padding-top: 10px;" class="social-media">
+         <a href="https://www.facebook.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/facebook.png" alt="facebook icon" style="width: 35px;">
+         </a>
+         <a href="https://www.google.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/google-plus.png" alt="google plus icon" style="width: 35px;">
+         </a>
+         <a href="https://www.instagram.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/ig.png" alt="instagram icon" style="width: 35px;">
+         </a>
+         <a href="https://www.pinterest.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/pinterest.png" alt="pinterest icon" style="width: 35px;">
+         </a>
+         <a href="https://www.linkedin.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/linkedin.png" alt="linkedin icon" style="width: 35px;">
+         </a>
+         <a href="https://twitter.com/" style="color: #4A4A4A; text-decoration: none; cursor: pointer; padding: 0 5px;">
+             <img src="image-assets/twitter.png" alt="twitter icon" style="width: 35px;">
+         </a>
+     </div>
+ </div>
+ <ul style="list-style-type: none; padding: 0 0 20px 0; font-size: 14px; text-align: center; padding-top: 20px;" class="navigation-footer">
+     <li style="display: inline-block; padding-right: 5px; margin-left: 0;">
+         <a href="" style="color: #4A4A4A; text-decoration: none; cursor: pointer;" class="remove-flex-mobile">Privacy Policy</a>
+     </li>
+     <li style="display: inline-block; padding-left: 5px; padding-right: 5px; margin-left: 0;">
+         <a href="" style="color: #4A4A4A; text-decoration: none; cursor: pointer;" class="remove-flex-mobile">Terms Of Use</a>
+     </li>
+     <li style="display: inline-block; padding-left: 5px; padding-right: 5px; margin-left: 0;">
+         <a href="" style="color: #4A4A4A; text-decoration: none; cursor: pointer;" class="remove-flex-mobile">Contact Us</a>
+     </li>
+     <li style="display: inline-block; padding-left: 5px; padding-right: 5px; margin-left: 0;">
+         <a href="" style="color: #4A4A4A; text-decoration: none; cursor: pointer;" class="remove-flex-mobile">Manage Email Preferrences</a>
+     </li>
+     <li style="display: inline-block; padding-left: 5px; margin-left: 0;">
+         <a href="" style="color: #4A4A4A; text-decoration: none; cursor: pointer;" class="remove-flex-mobile">Unsubscribe</a>
+     </li>
+ </ul>
---
+++ START/index.php
@@ -0,0 +1,99 @@
+<?php
+include 'inc/header.php';
+?>
+
+<body style="margin:0;">
+    <table style="border: none; margin: 0 auto ; padding: 0;">
+        <tr>
+            <td style="padding: 0; background-color: #FFFFFF;">
+
+                <div style="max-width: 600px; min-width: 200px; font-family: 'Arial', sans-serif; font-size: 16px; background-color: #FFFFFF; line-height: 1.4; color: #4A4A4A; border: 1px solid #DFDFDF; border-radius: 10px; overflow: hidden;">
+
+                    <div style="background-color: #EAAF47; height: 60px;">
+                    </div>
+
+                    <header style="text-align: center;">
+                        <h1 style="font-size: 58px;color: #EAAF47; background: #FFFFFF; margin-top: 30px; margin-bottom: 0; text-transform: uppercase;">LATEST NEWS</h1>
+                        <p style="margin: 0; padding-bottom: 30px; font-size:18px;">Stay in the loop. See what we're up to this month!</p>
+                    </header>
+
+                    <div style="padding: 0 20px;">
+                        <img src="image-assets/hero.jpg" alt="email hero image" style="display: block; background-size: cover; width: 100%;">
+                    </div>
+
+                    <h2 style="font-size: 32px; padding: 30px 20px 0 20px; margin-bottom: 0; text-transform: uppercase; color: #4A4A4A;">WHAT'S NEW</h2>
+
+                    <ul style="padding: 0 20px;">
+                        <li style="list-style-type: none; padding-bottom: 50px; margin-left: 0;">
+                            <div class="remove-flex-mobile" style="display:flex; justify-content:space-between;">
+                                <div style="flex-basis: 90%;" class="remove-flex-basis-mobile">
+                                    <img src="image-assets/img-1.jpg" alt="" style="display: block; width: 100%;">
+                                </div>
+                                <div style="flex-basis: 100%; padding-left: 30px;" class="remove-flex-basis-mobile">
+                                    <h3 style="text-transform: uppercase; font-size: 28px; margin-top: 0; margin-bottom: 10px; color: #4A4A4A" class="list-header">FOOTBALL SEASON IS HERE</h3>
+                                    <p style="padding-bottom: 20px; margin-top: 0; color: #4A4A4A">
+                                        Support our team. Catch the football games this season at the campus field.
+                                    </p>
+                                    <a href="" style="text-decoration: none; text-transform: uppercase; cursor: pointer; line-height: 1.1em; letter-spacing: 0; padding: 12px; background: #EAAF47; color: #4A4A4A; border-radius: 8px; text-align: center; font-size: 16px; font-weight: bold; box-sizing: border-box;">Read More</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li style="list-style-type: none; padding-bottom: 50px; margin-left: 0;">
+                            <div class="remove-flex-mobile" style="display:flex;">
+                                <div style="flex-basis: 90%;" class="remove-flex-basis-mobile">
+                                    <img src="image-assets/img-2.jpg" alt="" style="display: block; width: 100%;">
+                                </div>
+                                <div style="flex-basis: 100%; padding-left: 30px; width: 100%;" class="remove-flex-basis-mobile">
+                                    <h3 style="text-transform: uppercase; font-size: 28px; margin-top: 0; margin-bottom: 10px; color: #4A4A4A" class="list-header">DANCE TROUPE AUDITIONS</h3>
+                                    <p style="padding-bottom: 20px; margin-top: 0; color: #4A4A4A">
+                                        The dance troupe needs more members to join them as they perform for plays and other events this school year.
+                                    </p>
+                                    <a href="" style="text-decoration: none; text-transform: uppercase; cursor: pointer; line-height: 1.1em; letter-spacing: 0; padding: 12px; background: #EAAF47; color: #4A4A4A; border-radius: 8px; text-align: center; font-size: 16px; font-weight: bold; box-sizing: border-box;">Read More</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li style="list-style-type: none; padding-bottom: 50px; margin-left: 0;">
+                            <div class="remove-flex-mobile" style="display:flex; justify-content:space-between;">
+                                <div style="flex-basis: 90%;" class="remove-flex-basis-mobile">
+                                    <img src="image-assets/img-3.jpg" alt="" style="display: block; width: 100%;">
+                                </div>
+                                <div style="flex-basis: 100%; padding-left: 30px; width: 100%;" class="remove-flex-basis-mobile">
+                                    <h3 style="text-transform: uppercase; font-size: 28px; margin-top: 0; margin-bottom: 10px;" class="list-header">COLLEGE FAIR</h3>
+                                    <p style="padding-bottom: 20px; margin-top: 0;">
+                                        Attend our annual get-togethers and make new friends.
+                                    </p>
+                                    <a href="" style="text-decoration: none; text-transform: uppercase; cursor: pointer; line-height: 1.1em; letter-spacing: 0; padding: 12px; background: #EAAF47; color: #4A4A4A; border-radius: 8px; text-align: center; font-size: 16px; font-weight: bold; box-sizing: border-box;">Read More</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <!-- <li style="list-style-type: none; padding-bottom: 50px; margin-left: 0;">
+                            <div class="second-item-order" style="display:flex;">
+                                <div style="flex-basis: 100%; padding-right: 30px;" class="remove-flex-basis-mobile flex-order">
+                                    <h3 style="text-transform: uppercase; font-size: 28px; margin-top: 0; margin-bottom: 10px;"  class="list-header">DANCE TROUPE AUDITIONS</h3>
+                                    <p style="padding-bottom: 20px; margin-top: 0;">
+                                    The dance troupe needs more members to join them as they perform for plays and other events this school year.
+                                    </p>
+                                    <a href="" style="text-decoration: none; text-transform: uppercase; cursor: pointer; line-height: 1.1em; letter-spacing: 0; padding: 12px; background: #EAAF47; color: #4A4A4A; border-radius: 8px; text-align: center; font-size: 16px; font-weight: bold; box-sizing: border-box;">Read More</a>
+                                </div>
+                                <div style="flex-basis: 90%;" class="remove-flex-basis-mobile">
+                                    <img src="image-assets/img-3.jpg" alt="" style="display: block; width: 100%;">
+                                </div>                                
+                            </div>
+                        </li> -->
+                    </ul>
+
+                    <?php
+                    include 'inc/footer.php';
+                    ?>
+
+
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
