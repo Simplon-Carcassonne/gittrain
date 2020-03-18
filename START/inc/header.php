7742a7c3cdb967e6e2aa2dd69d1fd7e64fe3d048
Wazlyy <antoineplay11@gmail.com>
2020-03-18 11:54:19 +0100

    salut

--- START/inc/header.php
+++ START/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Oui, Peach top tier, oui.</title>
+    <title>Toad est meilleur que Peach désolé Kalvin !</title>
     <link href="css/style.css" rel="stylesheet">
 
 </head>
